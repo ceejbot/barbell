4514cf5f0c76338ce3d319cbe8124337b4d32653
C J Silverio <ceejceej@gmail.com>
2013-11-05 21:44:38 -0800

    Wrote a tiny amount of code.

--- www/assets/main.js
+++ www/assets/main.js
@@ -1,30 +1,35 @@
-// initialize Hoodie
-var hoodie  = new Hoodie()
+var hoodie  = new Hoodie();
 
-// initial load of all todo items from the store
-hoodie.store.findAll('todo').then( function(todos) {
-  todos.sort( sortByCreatedAt ).forEach( addTodo )
-})
+hoodie.store.findAll('workout')
+.then(function(workouts)
+{
+	workouts.sort(sortByDate).forEach(addWorkout);
+});
 
-// when a new todo gets stored, add it to the UI
-hoodie.store.on('add:todo', addTodo)
-// clear todo list when the get wiped from store
-hoodie.account.on('signout', clearTodos)
+function sortByDate(left, right)
+{
+	return left.created > right.created;
+}
+
+function addWorkout(workout)
+{
+	$('#workouts').append('<li>' + workout.created + '</li');
+}
+
+function clearWorkouts()
+{
+	$('#workouts').html('');
+}
+
+hoodie.store.on('add:workout', addWorkout);
+hoodie.account.on('signout', clearWorkouts);
 
 // handle creating a new task
-$('#todoinput').on('keypress', function(event) {
-  if (event.keyCode == 13) { // ENTER
-    hoodie.store.add('todo', {title: event.target.value});
-    event.target.value = '';
-  }
+$('#todoinput').on('keypress', function(event)
+{
+	if (event.keyCode == 13) // ENTER
+	{
+		hoodie.store.add('workout', { title: event.target.value });
+		event.target.value = '';
+	}
 })
-
-function addTodo( todo ) { 
-  $('#todolist').append('<li>'+todo.title+'</li>');
-}
-function clearTodos() {
-  $('#todolist').html('');
-}
-function sortByCreatedAt(a, b) { 
-  return a.createdAt > b.createdAt
-}
